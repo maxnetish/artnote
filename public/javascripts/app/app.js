0c69879617f9fc9d4217e54f7ad02046ba00669d
Maksim Gordeev <root@microsoft>
2014-06-09 17:19:24 +0400

    add directive (date editor) & filter (date view)

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -31,6 +31,41 @@ angular.module('artbookApp',
                     return formatted;
                 });
             }
-        }
+        };
+    })
+    .directive("artDateEditor", function ($filter) {
+        return {
+            require: 'ngModel',
+            link: function (scope, element, attrs, ngModel) {
+                if (!ngModel) {
+                    return;
+                }
+                ngModel.$parsers.unshift(
+                    function (viewValue) {
+                        var date = new Date(viewValue);
+                        if (isNaN(date.getTime())) {
+                            // invalid date:
+                            ngModel.$setValidity('artDate', false);
+                            return null;
+                        }
+                        ngModel.$setValidity('artDate', true);
+                        return date;
+                    });
+                ngModel.$formatters.unshift(function (v) {
+                    var formatted = $filter('date')(v, 'yyyy-MM-dd');
+                    return formatted;
+                });
+            }
+        };
+    })
+    .filter('artdate', function () {
+        return function (input) {
+            // input will be Date
+            if (angular.isDate(input)) {
+                return input.toLocaleDateString();
+            } else {
+                return input;
+            }
+        };
     });
 
